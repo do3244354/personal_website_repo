4036722c59f7ef23bfb305edd4788a5fdbca6365
do3244354 <dorothee.grondin.1@ulaval.ca>
2025-03-04 20:57:30 -0500

    canvas added to website - do not push on github

---
+++ sketch.js
@@ -0,0 +1,10 @@
+let winHeight = 400;
+let winWidth = 600;
+
+function setup(){
+	let canvas = createCanvas(winWidth, winHeight);
+	canvas.parent("sketch-container");
+}
+function draw() {
+  background(100);
+}
