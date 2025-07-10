7bfa1c13945d96698269e22037d91749125aff51
dorotheeGrondin <dorothee.grondin.1@ulaval.ca>
2025-07-10 12:34:01 -0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,6 +1,15 @@
 let pi = 3.141592654;
 let radius = 100;
 
+let WIDTH = 600;
+let HEIGHT = 400;
+
+//list to store the circle's centers
+puncture_list = []
+//variable to store the overlapped value
+overlapped = null
+clicked = null
+
 function setup() {
   canvas = createCanvas(600, 400);
   canvas.parent("sketch-container");
@@ -11,9 +20,24 @@ function setup() {
 function draw() {
   update_interface();
   //just a small test
-  if (40 < mouseY && mouseY < 360){
+  if ((40 < mouseY && mouseY < 360) && (mouseIsPressed == true)){
     ellipse(mouseX, mouseY,15,15);
   };
+  for (i = 0; i < puncture_list.length ; i++){
+    fill("red")
+    stroke("red")
+    ellipse(WIDTH/2+puncture_list[i][0]*HEIGHT, HEIGHT/2+puncture_list[i][1]*HEIGHT, 8, 8)
+    if (overlapped == i){
+      fill("#B31F1E")
+      stroke("#B31F1E")
+      ellipse(WIDTH/2+puncture_list[i][0]*HEIGHT, HEIGHT/2+puncture_list[i][1]*HEIGHT, 8, 8)
+    }
+    if (clicked == i){
+      fill("#B31F1E")
+      stroke("#B31F1E")
+      ellipse(WIDTH/2+puncture_list[i][0]*HEIGHT, HEIGHT/2+puncture_list[i][1]*HEIGHT, 12, 12)
+    }
+  }
   side = SideEntry.value();
   point_around_circle(side);
 }
@@ -64,6 +88,7 @@ function draw_interface() {
   //PunctureButton.position(213, 375);
   PunctureButton.position(170, -30, "relative");
   PunctureButton.size(25,20);
+  PunctureButton.mousePressed(addPuncture)
 
   text("Add component: ", 260, 388)
   ComponentButton = createButton("+");
@@ -93,4 +118,64 @@ function update_interface(){
   fill(280);
   stroke(200);
   ellipse(300, 200, radius*2, radius*2);
+}
+
+function mouseMoved(){
+  counter = 0; 
+  is_clicked = false;
+  for (i = 0; i < puncture_list.length; i++){
+    if (dist(mouseX, mouseY, WIDTH/2+puncture_list[i][0]*HEIGHT, HEIGHT/2+puncture_list[i][1]*HEIGHT) < 0.009*HEIGHT){
+      overlapped = i;
+      if (mouseIsPressed == true){
+        clicked = i;
+        is_clicked = true;
+      }
+    }
+    else{
+      counter += 1
+    }  
+  if (counter == puncture_list.length){
+      overlapped = null;
+  }
+  if (is_clicked == false){
+      clicked = null;
+  }
+  }
+  //if (clicked != null){
+    //puncture_list[clicked][0] = (movedX - WIDTH/2)/(HEIGHT);
+    //puncture_list[clicked][1] = (movedY - HEIGHT/2)/(HEIGHT);
+  //}
+}
+
+function mousePressed(){
+  is_clicked = false;
+  for (i = 0; i < puncture_list.length; i++){
+    if (dist(mouseX, mouseY, WIDTH/2+puncture_list[i][0]*HEIGHT, HEIGHT/2+puncture_list[i][1]*HEIGHT) < 0.009*HEIGHT){
+      clicked = i;
+      is_clicked = true;
+    }
+  if (is_clicked == false){
+      clicked = null;
+    }
+  }
+}
+
+function mouseReleased(){
+  clicked = null;
+}
+
+function addPuncture(){
+  puncture_list.push([0, 0])
+}
+
+function mouseDragged(){
+  if (clicked != null){
+    print(WIDTH/2)
+    print(HEIGHT)
+    print((mouseX - WIDTH/2)/(HEIGHT))
+    if (40 < mouseY && mouseY < 360){
+      puncture_list[clicked][0] = (mouseX - WIDTH/2)/(HEIGHT)
+      puncture_list[clicked][1] = (mouseY - HEIGHT/2)/(HEIGHT)
+    }
+  }
 }
